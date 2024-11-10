e62cbcfeba55e392b32f2350b44093b02c6dbcb3
GianPolastri <gianpolastri@gmial.com>
2024-11-10 00:54:57 -0300

    Funciones add y get de Ciudades

--- tp2/src/main/java/aed/Ciudades.java
+++ tp2/src/main/java/aed/Ciudades.java
@@ -1,9 +1,36 @@
 package aed;
 
 public class Ciudades {
-    int id;
-    int earnings;
-    int losses;
-    int superavit;
+    private int id;
+    private int earnings;
+    private int losses;
+    private int superavit;
     
+    public Ciudades(int id){
+        this.id = id;
+        this.earnings = 0;
+        this.losses = 0;
+        this.superavit = 0;
+    }
+
+    public void addEarnings(int e){
+        this.earnings = this.earnings + e;
+    }
+
+    public void addLosses(int l){
+        this.losses = this.losses + l;
+    }
+
+    public int getEarning(){
+        return this.earnings;
+    }
+
+    public int getLosses(){
+        return this.losses;
+    }
+
+    public int getSuperavit(){
+        this.superavit = this.earnings - this.losses;
+        return this.superavit;
+    }
 }
