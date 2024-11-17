e46fa049178e4f6ee6b7a694cbc311ebee6045f7
Nicalve <calvenico2@gmail.com>
2024-11-17 19:31:06 -0300

    superheap terminado!

--- tp2/src/main/java/aed/Superheap.java
+++ tp2/src/main/java/aed/Superheap.java
@@ -1,31 +1,49 @@
 package aed;
 
-public class Superheap {
-    private Heap<Handle> reditoHeap;
-    private Heap<Handle> antiguedadHeap;
+import java.util.Comparator;
 
-    public Superheap() {
-        this.reditoHeap = new Heap<>( (h1, h2) -> Integer.compare(h1.getTraslado().gananciaNeta, h2.getTraslado().gananciaNeta) );
-        
-        this.antiguedadHeap = new Heap<>( (h1, h2) -> Integer.compare(h1.getTraslado().timestamp, h2.getTraslado().timestamp) );
+public class Superheap {
+    public  Heap<Handle> reditoHeap;
+    public Heap<Handle> antiguedadHeap;
+    private class reditoComparator implements Comparator<Handle>{
+    @Override
+        public int compare(Handle h1, Handle h2){
+            return Integer.compare(h1.getTraslado().gananciaNeta, h2.getTraslado().gananciaNeta);
+        }
+    }
+    private class antiguedadComparator implements Comparator<Handle>{
+        @Override
+        public int compare(Handle h1, Handle h2){
+            return Integer.compare(h1.getTraslado().timestamp, h2.getTraslado().timestamp);
+        }
     }
 
-    public void encolar(Traslado traslado) {
-        Handle mihandle = new Handle(traslado);
+    public Superheap() {
+        reditoComparator reditocomparador = new reditoComparator();
+        antiguedadComparator antiguedadcomparador = new antiguedadComparator();
+        this.reditoHeap = new Heap<>(reditocomparador);
+        
+        this.antiguedadHeap = new Heap<>( antiguedadcomparador );
+    }
+
+    public void superencolar(Traslado traslado) {
+        Handle mihandle = new Handle(traslado); 
         Handle otroHandle = new Handle(traslado);
-        mihandle.setPosPropioHeap(reditoHeap.tamaño()); 
-        int pos = reditoHeap.encolar(mihandle); 
+        int pos = reditoHeap.encolar(mihandle);
+        mihandle.setPosPropioHeap(pos);
         otroHandle.setPosOtroHeap(pos);
+        actualizarPosEnElOtro(reditoHeap, antiguedadHeap);
         int posotro = antiguedadHeap.encolar(otroHandle);
+        otroHandle.setPosPropioHeap(posotro);
         mihandle.setPosOtroHeap(posotro);
-        actualizarPosEnElOtro(antiguedadHeap, reditoHeap);
+       actualizarPosEnElOtro(antiguedadHeap, reditoHeap);
     }
 
 
     public Traslado desencolarRedito(){
         Handle desencolado = (Handle) reditoHeap.desencolarRaiz();
         actualizarPosEnElOtro(reditoHeap,antiguedadHeap);
-        antiguedadHeap.desencolar(desencolado.getPosOtroHeap());
+        Handle nashei = antiguedadHeap.desencolar(desencolado.getPosOtroHeap());
         actualizarPosEnElOtro(antiguedadHeap, reditoHeap);
         return desencolado.getTraslado();
 
@@ -34,8 +52,8 @@ public class Superheap {
 
     public Traslado desencolarAntiguedad(){
         Handle desencolado = (Handle) antiguedadHeap.desencolarRaiz();
-        actualizarPosEnElOtro(antiguedadHeap,reditoHeap);
         reditoHeap.desencolar(desencolado.getPosOtroHeap());
+        actualizarPosEnElOtro(antiguedadHeap,reditoHeap);
         actualizarPosEnElOtro(reditoHeap, antiguedadHeap);
         return desencolado.getTraslado();
 
@@ -43,10 +61,12 @@ public class Superheap {
     public void actualizarPosEnElOtro(Heap h1,Heap h2){
         for(int i = 0; i<h1.tamaño();i++){
             Handle actual = (Handle)h1.get(i);
-            int posactual = actual.getPosPropioHeap();
             int posenelotro = actual.getPosOtroHeap();
+            actual.setPosPropioHeap(i);
+            if (posenelotro< h2.tamaño() && posenelotro > -1){
             Handle otro = (Handle)h2.get(posenelotro);
-            otro.setPosOtroHeap(posactual);
+            otro.setPosOtroHeap(i);
+            }
         }
     }
 }
